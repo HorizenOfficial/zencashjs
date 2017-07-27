e54847def98f1c283c941af1915c29ed0dba7407
aayanl <aayanzillis@gmail.com>
2017-07-27 17:13:24 -0400

    Fix multisig and splice buffer correctly

--- src/transaction.js
+++ src/transaction.js
@@ -106,15 +106,13 @@ function mkPubkeyHashReplayScript (address: string): string {
   var blockHeightBuffer = Buffer.alloc(4)
   blockHeightBuffer.writeUInt32LE(blockHeight, 0)
   if (blockHeightBuffer[3] === 0x00) {
-    var temp_buf = new Buffer(3)
-    temp_buf.fill(blockHeightBuffer, 0, 3)
-    blockHeightBuffer = temp_buf
+    blockHeightBuffer = blockHeightBuffer.slice(0, 3)
   }
-  var blockHeightHex = blockHeightBuffer.toString('hex')  
+  var blockHeightHex = blockHeightBuffer.toString('hex')
 
   // Need to reverse it
   var blockHash = '00000001cf4e27ce1dd8028408ed0a48edd445ba388170c9468ba0d42fff3052'
-  var blockHashHex = Buffer.from(blockHash, 'hex').reverse().toString('hex')  
+  var blockHashHex = Buffer.from(blockHash, 'hex').reverse().toString('hex')
 
   // '14' is the length of the subAddrHex (in bytes)
   return (
@@ -140,8 +138,33 @@ function mkPubkeyHashReplayScript (address: string): string {
 function mkScriptHashScript (address: string): string {
   var addrHex = bs58check.decode(address).toString('hex')
   var subAddrHex = addrHex.substring(4, addrHex.length) // Cut out the '00' (we also only want 14 bytes instead of 16)
+
+  // TODO: change this so it gets block hash and height via REST API
+  var blockHeight = 142091
+
+  var blockHeightBuffer = Buffer.alloc(4)
+  blockHeightBuffer.writeUInt32LE(blockHeight, 0)
+  if (blockHeightBuffer[3] === 0x00) {
+    blockHeightBuffer = blockHeightBuffer.slice(0, 3)
+  }
+  var blockHeightHex = blockHeightBuffer.toString('hex')
+
+  // Need to reverse it
+  var blockHash = '00000001cf4e27ce1dd8028408ed0a48edd445ba388170c9468ba0d42fff3052'
+  var blockHashHex = Buffer.from(blockHash, 'hex').reverse().toString('hex')
+
   // '14' is the length of the subAddrHex (in bytes)
-  return zopcodes.OP_HASH160 + '14' + subAddrHex + zopcodes.OP_EQUAL
+  return (
+    zopcodes.OP_HASH160 +
+    '14' +
+    subAddrHex +
+    zopcodes.OP_EQUAL +
+    getStringBufferLength(blockHashHex) +
+    blockHashHex +
+    getStringBufferLength(blockHeightHex) +
+    blockHeightHex +
+    zopcodes.OP_CHECKBLOCKATHEIGHT
+  )
 }
 
 /*
@@ -172,7 +195,7 @@ function signatureForm (
   hashcode: number
 ): TXOBJ {
   // Copy object so we don't rewrite it
-  var newTx = JSON.parse(JSON.stringify(txObj));  
+  var newTx = JSON.parse(JSON.stringify(txObj));
 
   for (var j = 0; j < newTx.ins.length; j++) {
     newTx.ins[j].script = ''
@@ -302,29 +325,29 @@ function signTx (
   // Prepare our signature
   const signingTx: TXOBJ = signatureForm(txObj, i, script, hashcode)
   const signingTxHex: string = serializeTx(signingTx) // Convert to hex string
-  const signingTxWithHashcode = signingTxHex + _buf16.toString('hex')  
+  const signingTxWithHashcode = signingTxHex + _buf16.toString('hex')
   const msg = zcrypto.sha256x2(Buffer.from(signingTxWithHashcode, 'hex'))
   const rawsig = secp256k1
     .sign(Buffer.from(msg, 'hex'), Buffer.from(privKey, 'hex'))
     .signature.toString('hex')
 
-  // Encode signature  
+  // Encode signature
   var b1 = rawsig.substr(0, 64)
-  var b2 = rawsig.substr(64, 128)  
+  var b2 = rawsig.substr(64, 128)
 
   if ('89abcdef'.indexOf(b1[0]) != -1) {
-    b1 = '00' + b1  
+    b1 = '00' + b1
   }
   if ('89abcdef'.indexOf(b2[0]) != -1) {
-    b2 = '00' + b2    
-  }    
+    b2 = '00' + b2
+  }
 
   // http://www.righto.com/2014/02/bitcoins-hard-way-using-raw-bitcoin.html
   // 02 is the integer, 30 is the sequence
   var left = '02' + getStringBufferLength(b1) + b1
-  var right = '02' + getStringBufferLength(b2) + b2  
+  var right = '02' + getStringBufferLength(b2) + b2
   const sig = '30' + getStringBufferLength(left + right) + left + right
-  const sigAndHashcode = sig + Buffer.from([hashcode]).toString('hex')  
+  const sigAndHashcode = sig + Buffer.from([hashcode]).toString('hex')
 
   // Chuck it back into txObj
   txObj.ins[i].script =
