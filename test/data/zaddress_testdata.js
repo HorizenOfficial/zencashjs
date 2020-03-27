74df4e569045a048db6ac22c9b71b134ca21938b
Paolo Cappelletti <paolo@horizenlabs.io>
2020-03-27 13:44:31 +0100

    improved unit test for zSecretKeyToTransmissionKey

---
+++ test/data/zaddress_testdata.js
@@ -0,0 +1,8 @@
+module.exports = {
+  keys: [
+    {
+        zSecretkey: "0c10a61a669bc4a51000c4c74ff58c151912889891089f7bae5e4994a73af7a8",
+        pk: "22d666c34ababacf6a9a4a752cc7870b505b64e85638aa45d23ac32992397960"
+    }
+  ]
+}
