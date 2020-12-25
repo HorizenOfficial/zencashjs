a52a8405df0cad7cafe0fa1a74c161799addc640
jonahion <jonahion@gmail.com>
2020-12-25 19:49:31 +0100

    Update specs

--- test/check-lib-sync.js
+++ test/check-lib-sync.js
@@ -1,30 +1,32 @@
 const expect = require('chai').expect
 const { exec, execSync } = require('child_process')
 
-const makeTmpChange = dir => execSync(`touch ${dir}/tmp.js && git add --intent-to-add ${dir}/tmp.js`)
-const resetTmpChange = dir => execSync(`git reset ${dir}/tmp.js && rm ${dir}/tmp.js`)
-
-it('runs successfully when there are no changes between the current and main branches', done => {
-    exec('./scripts/check-lib-sync.sh', error => {
-        expect(error).to.equal(null)
-        done()
+describe('scripts/check-lib-sync.sh', () => {
+    const addTemporaryChange = dir => execSync(`touch ${dir}/tmp.js && git add --intent-to-add ${dir}/tmp.js`)
+    const revertTemporaryChange = dir => execSync(`git reset ${dir}/tmp.js && rm ${dir}/tmp.js`)
+    
+    it('runs successfully when there are no changes between the current and main branches', done => {
+        exec('scripts/check-lib-sync.sh', error => {
+            expect(error).to.equal(null)
+            done()
+        })
     })
-})
 
-it('runs successfully when ./lib is in sync with ./src', done => {
-    ['src', 'lib'].forEach(dir => makeTmpChange(dir))
-    exec('./scripts/check-lib-sync.sh', error => {
-        ['src', 'lib'].forEach(dir => resetTmpChange(dir))
-        expect(error).to.equal(null)
-        done()
+    it('runs successfully when ./lib is in sync with ./src', done => {
+        ['src', 'lib'].forEach(dir => addTemporaryChange(dir))
+        exec('scripts/check-lib-sync.sh', error => {
+            ['src', 'lib'].forEach(dir => revertTemporaryChange(dir))
+            expect(error).to.equal(null)
+            done()
+        })
     })
-})
 
-it('errors when ./lib is not in sync with ./src', done => {
-    makeTmpChange('src')
-    exec('./scripts/check-lib-sync.sh', error => {
-        resetTmpChange('src')
-        expect(error).to.be.an('Error')
-        done()
-    })
+    it('errors when ./lib is not in sync with ./src', done => {
+        addTemporaryChange('src')
+        exec('scripts/check-lib-sync.sh', error => {
+            revertTemporaryChange('src')
+            expect(error).to.be.an('Error')
+            done()
+        })
+    })  
 })
