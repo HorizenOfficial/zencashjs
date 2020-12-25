59aacce5835f8ad5855cec8c151226a6f7404fb4
jonahion <jonahion@gmail.com>
2020-12-25 21:58:29 +0100

    Pass target branch from Travis env

--- scripts/check-lib-sync.sh
+++ scripts/check-lib-sync.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 diff_in_dir() {
-  DIFF=$(git diff --name-only origin/master "$1")
+  TARGET_BRANCH=${TARGET_BRANCH:-master}
+  DIFF=$(git diff --name-only origin/"$TARGET_BRANCH" "$1")
   for FILE in $DIFF; do
     basename "$FILE"
   done
