2aff271d7f44b35edf5d458199d58f69ba2c2dde
jonahion <jonahion@gmail.com>
2020-12-25 18:37:15 +0100

    Re-implement the ./lib sync check to use git only; remove husky dependency

---
+++ scripts/check_lib_sync.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+diff_in_dir() {
+  DIFF=$(git diff --name-only origin/master "$1")
+  for FILE in $DIFF; do
+    basename $FILE
+  done
+}
+
+echo Checking if ./lib is in sync with ./src...
+SRC_DIFF=$(diff_in_dir src)
+LIB_DIFF=$(diff_in_dir lib)
+
+[[ "$SRC_DIFF" == "$LIB_DIFF" ]] && exit 0
+exit 1
---
+++ test/check_lib_sync.js
@@ -0,0 +1,30 @@
+const expect = require('chai').expect
+const { exec, execSync } = require('child_process')
+
+const makeTmpChange = dir => execSync(`touch ${dir}/tmp.js && git add --intent-to-add ${dir}/tmp.js`)
+const resetTmpChange = dir => execSync(`git reset ${dir}/tmp.js && rm ${dir}/tmp.js`)
+
+it('runs successfully when there are no changes between the current and main branches', done => {
+    exec('./scripts/check_lib_sync.sh', error => {
+        expect(error).to.equal(null)
+        done()
+    })
+})
+
+it('runs successfully when ./lib is in sync with ./src', done => {
+    ['src', 'lib'].forEach(dir => makeTmpChange(dir))
+    exec('./scripts/check_lib_sync.sh', error => {
+        ['src', 'lib'].forEach(dir => resetTmpChange(dir))
+        expect(error).to.equal(null)
+        done()
+    })
+})
+
+it('errors when ./lib is not in sync with ./src', done => {
+    makeTmpChange('src')
+    exec('./scripts/check_lib_sync.sh', error => {
+        resetTmpChange('src')
+        expect(error).to.be.an('Error')
+        done()
+    })
+})
