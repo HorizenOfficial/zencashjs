ca362217e9dfc5844133cbc745b8ec01e7bfc369
Yuriko Inaba <yurikoainaba@gmail.com>
2021-09-17 09:31:23 -0400

    Update txobj for new tx types and cert

--- src/types.js
+++ src/types.js
@@ -1,15 +1,80 @@
 // @flow
 
 export type TXOBJ = {
-  locktime: number,
-  version: number,
-  ins: {
-    output: { hash: string, vout: number },
-    script: string,
-    sequence: string,
-    prevScriptPubKey: string,
+  nLockTime: number,
+  version: number, 
+  inputs: {
+    prevTxId: string,
+    sequenceNumber: string,
+    outputIndex: number,
+    script: string, // check
+    prevScriptPubKey: string, // check
   }[],
-  outs: { script: string, satoshis: number }[],
+  outputs: { 
+    script: string,
+    satoshis: number 
+  }[],
+  joinsplit?: {}[], // Does ZencashJS support shielded txn?
+  sc_params?: {
+    vsc_ccout: {
+      epoch_length: number,
+      satoshis: number,
+      address: string,
+      customData: string,
+      wCertVk: string,
+      vFieldElementCertificateFieldConfig: number[],
+      vBitVectorCertificateFieldConfig: number[][],
+      forwardTransferScFee: number,
+      mainchainBackwardTransferScFee: number,
+      mbtrRequestDataLength: number
+    }[],
+    vft_ccout: {
+      scid: string,
+      satoshis: number,
+      address: string
+    }[],
+    vcsw_ccin: {
+      value: number,
+      scId: string,
+      nullifer: string,
+      scProof: string,
+      actCertDataHash: string,
+      ceasingCumScTxCommTree: string,
+      redeemScript: string,
+      pubKeyHash: string
+    }[],
+    vmbtr_out: {
+      scid: string,
+      scFee: string,
+      mcDestinationAddress: string,
+      vScRequestData: string[]
+    }[],
+  },
+}
+
+export type CERTIFICATE_OBJ = {
+  hash: string,
+  version: number,
+  inputs: {
+    prevTxId: string,
+    sequenceNumber: string,
+    outputIndex: number 
+  }[],
+  sidechainId: string,
+  epochNumber: number,
+  quality: number,
+  endEpochCumScTxCommTreeRoot: number,
+  scProof: string,
+  vFieldElementCertificateField: string[],
+  vBitVectorCertificateField: string[],
+  ftScFee: number,
+  mbtrScFee: number,
+  outputs: { 
+    script: string,
+    satoshis: number,
+    isFromBackwardTransfer: boolean,
+    pubKeyHash: string
+  }[],
 }
 
 // HISTORY Structure
