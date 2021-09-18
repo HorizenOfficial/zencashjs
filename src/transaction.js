3132ed1ff8daeb08dd888067ae6b778802b1ce3e
Yuriko Inaba <yurikoainaba@gmail.com>
2021-09-17 22:09:06 -0400

    Deserialize Certificates

--- src/transaction.js
+++ src/transaction.js
@@ -204,12 +204,76 @@ function deserializeTx (hexStr: string, withPrevScriptPubKey: boolean = false): 
   var txObj = { version: 0, locktime: 0, ins: [], outs: [] }
 
   // Version
-  txObj.version = buf.readUInt32LE(offset)
+  const version = buf.readInt32LE(offset);
+  txObj.version = version;
   offset += 4
+
+  // Certificate
+  if (version === -5) {
+    const sidechainId = buf.slice(offset, offset + 32).reverse().toString('hex');
+    offset += 32
+
+    const epochNumber = buf.readInt32LE(offset);
+    offset += 4 
+
+    const quality = zbufferutils.readUInt64LE(buf, offset)
+    offset += 8; 
+
+    const endEpochCumScTxCommTreeRootLen = varuint.decode(buf, offset);
+    offset += varuint.decode.bytes
+    const endEpochCumScTxCommTreeRoot = buf.slice(offset, offset + endEpochCumScTxCommTreeRootLen).toString('hex'); //come back to this one
+    offset += endEpochCumScTxCommTreeRootLen
+
+    const scProofLen = varuint.decode(buf, offset);
+    offset += varuint.decode.bytes
+    const scProof = buf.slice(offset, offset + scProofLen).toString('hex');
+    offset += scProofLen
+
+    const vFieldElementCertificateFieldLen = varuint.decode(buf, offset);
+    offset += varuint.decode.bytes
+    let vFieldElementCertificateField = [];
+    for (let i = 0; i < vFieldElementCertificateFieldLen; i++) {
+      let fieldLen = varuint.decode(buf, offset);
+      offset += varuint.decode.bytes
+
+      vFieldElementCertificateField.push(buf.slice(offset, offset + fieldLen).toString('hex'));
+      offset += fieldLen
+    }
+
+    const vBitVectorCertificateFieldLen = varuint.decode(buf, offset);
+    offset += varuint.decode.bytes
+    let vBitVectorCertificateField = [];
+    for (let i = 0; i < vBitVectorCertificateFieldLen; i++) {
+      let fieldLen = varuint.decode(buf, offset);
+      offset += varuint.decode.bytes
+
+      vBitVectorCertificateField.push(buf.slice(offset, offset + fieldLen).toString('hex'));
+      offset += fieldLen
+    }
+
+    const ftScFee = zbufferutils.readUInt64LE(buf, offset);
+    offset += 8;
+    
+    const mbtrScFee = zbufferutils.readUInt64LE(buf, offset)
+    offset += 8; 
+    
+    txObj.cert = {
+      sidechainId,
+      epochNumber,
+      quality,
+      endEpochCumScTxCommTreeRoot,
+      scProof,
+      vFieldElementCertificateField,
+      vBitVectorCertificateField,
+      ftScFee,
+      mbtrScFee,
+    }
+  }
 
   // Vins
   var vinLen = varuint.decode(buf, offset)
   offset += varuint.decode.bytes
+
   for (let i = 0; i < vinLen; i++) {
     // Else its
     const hash = buf.slice(offset, offset + 32)
@@ -264,9 +328,34 @@ function deserializeTx (hexStr: string, withPrevScriptPubKey: boolean = false): 
     })
   }
 
+  // Backward transfer outputs
+  if (version === -5) {
+    // copied from above.
+    var voutLen = varuint.decode(buf, offset)
+    offset += varuint.decode.bytes
+    for (let i = 0; i < voutLen; i++) {
+      const satoshis = zbufferutils.readUInt64LE(buf, offset)
+      offset += 8
+  
+      const scriptLen = varuint.decode(buf, offset)
+      offset += varuint.decode.bytes
+  
+      const script = buf.slice(offset, offset + scriptLen)
+      offset += scriptLen
+  
+      txObj.outs.push({
+        satoshis: satoshis,
+        script: script.toString('hex'), // check if this is the same
+        isFromBackwardTransfer: true
+      })
+    }
+  }
+
+  if (version != -5) {
   // Locktime
   txObj.locktime = buf.readInt32LE(offset)
   offset += 4
+  }
 
   return txObj
 }
