3fc8f532c319bd11d3f9172e39cba643cca907b4
Yuriko Inaba <yurikoainaba@gmail.com>
2021-09-22 07:41:06 -0400

    Update txobj for new tx types and cert (#55)
    * Update txobj for new tx types and cert
    
    * update
    
    * fixes and move certificate
    
    * format
    
    * make locktime optional (for certs)
    
    * other fixes
    
    * scId consistent
    
    * make camelCase
    
    * make sure shape matches
    
    * add back mcReturnAddress
    
    * make scid consistent w bitcore-lib-zen

--- src/types.js
+++ src/types.js
@@ -1,7 +1,8 @@
 // @flow
 
+// Shape for sc_params and cert should stay consistent with Zendoo and bitcore-lib-zen
 export type TXOBJ = {
-  locktime: number,
+  locktime?: number,
   version: number,
   ins: {
     output: { hash: string, vout: number },
@@ -9,7 +10,56 @@ export type TXOBJ = {
     sequence: string,
     prevScriptPubKey: string,
   }[],
-  outs: { script: string, satoshis: number }[],
+  outs: { script: string, satoshis: number, isFromBackwardTransfer?: boolean, pubKeyHash?: string }[],
+  sc_params?: {
+    vsc_ccout: {
+      epoch_length: number,
+      satoshis: number,
+      address: string,
+      customData: string,
+      certVk: string,
+      constantData: string,
+      wCeasedVk: string,
+      vFieldElementCertificateFieldConfig: number[],
+      vBitVectorCertificateFieldConfig: number[][],
+      forwardTransferScFee: number,
+      mainchainBackwardTransferScFee: number,
+      mbtrRequestDataLength: number
+    }[],
+    vft_ccout: {
+      scid: string,
+      satoshis: number,
+      address: string,
+      mcReturnAddress: string
+    }[],
+    vcsw_ccin: {
+      value: number,
+      scId: string,
+      nullifer: string,
+      scProof: string,
+      actCertDataHash: string,
+      ceasingCumScTxCommTree: string,
+      redeemScript: string,
+      pubKeyHash: string
+    }[],
+    vmbtr_out: {
+      scid: string,
+      scFee: number,
+      mcDestinationAddress: string,
+      vScRequestData: string[]
+    }[],
+  },
+  cert?: {
+    sidechainId: string,
+    epochNumber: number,
+    quality: number,
+    endEpochCumScTxCommTreeRoot: string,
+    scProof: string,
+    vFieldElementCertificateField: string[],
+    vBitVectorCertificateField: string[],
+    ftScFee: number,
+    mbtrScFee: number,
+  }
 }
 
 // HISTORY Structure
