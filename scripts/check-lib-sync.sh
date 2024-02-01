3f14b2b6555540628e6426995cc15571853eba86
cronicc <cronic@zensystem.io>
2024-02-01 13:00:06 +0000

    Only compare changed files in src/ against lib/

--- scripts/check-lib-sync.sh
+++ scripts/check-lib-sync.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+set -eEuo pipefail
+
 diff_in_dir() {
-  TARGET_BRANCH=${TARGET_BRANCH:-master}
-  DIFF=$(git diff --name-only origin/"$TARGET_BRANCH" "$1")
-  for FILE in $DIFF; do
+  TARGET_BRANCH="${TARGET_BRANCH:-master}"
+  DIFF="$(git diff --name-only origin/"$TARGET_BRANCH" "$1")"
+  while read -r FILE; do
     # Ignore changes to src/types.js because it only has Flow type annotations and it does not compile to any vanilla Javascript
-    if [[ "$FILE" != "src/types.js" ]]; then
+    if [ "$FILE" != "src/types.js" ]; then
       basename "$FILE"
     fi
-  done
+  done <<< "${DIFF}"
 }
 
 echo Checking if ./lib is in sync with ./src...
-SRC_DIFF=$(diff_in_dir src)
-LIB_DIFF=$(diff_in_dir lib)
+SRC_DIFF="$(diff_in_dir src)"
+LIB_DIFF="$(diff_in_dir lib)"
 
-[[ "$SRC_DIFF" == "$LIB_DIFF" ]] && exit 0
-exit 1
+while read -r line; do
+  grep -q "${line}" <<< "${LIB_DIFF}"
+done <<< "${SRC_DIFF}"
